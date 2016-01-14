35f36f9e693cf910ebabdc7adc1808ed82b8e155
Matevz Vucnik <vucnikm@gmail.com>
2016-01-14 15:30:55 +0100

    add proxy code

--- videk-proxy.py
+++ videk-proxy.py
@@ -5,6 +5,7 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from urlparse import urlparse, parse_qs
 from videk_rest_client import Videk
 from datetime import datetime
+from proxydatabase import ProxyDatabase
 
 server_port = 8080
 videk_api_url = "http://localhost:3000/api"
@@ -22,6 +23,9 @@ def main():
 		server.socket.close()
 
 class requestHandler(BaseHTTPRequestHandler):
+
+	sensor = ProxyDatabase("sensors")
+	table = ProxyDatabase("tables")
 
 	def upload_data(self, cluster, node, sensor_t, \
 					sensor_q, sensor_u, measurements):
@@ -50,8 +54,8 @@ class requestHandler(BaseHTTPRequestHandler):
 		for measurement in measurements:
 			data = json.loads(videk_m)
 			data['value'] = measurement["v"]
-			data['ts'] = datetime.fromtimestamp(measurement["t"]) \
-						.isoformat()
+			data['ts'] = datetime.fromtimestamp(int(measurement["t"])) \
+				.isoformat()
 			data['latitude'] = measurement["lat"]
 			data['longitude'] = measurement["lon"]
 			preparedData.append(data)
@@ -61,8 +65,45 @@ class requestHandler(BaseHTTPRequestHandler):
 	def do_POST(self):
 		content_length = int(self.headers['Content-Length'])
 		content = self.rfile.read(content_length)
-		try:
-			data = json.loads(content)
+		url = urlparse(self.path)
+		resource = url.path
+		params = parse_qs(url.query)
+
+		if resource == "/reg-s":
+			self.send_response(200)
+			self.end_headers()
+			message = self.sensor.store(content)
+			self.wfile.write(message)
+			return
+
+		if resource == "/reg-t":
+			self.send_response(200)
+			self.end_headers()
+			message = self.table.store(content)
+			self.wfile.write(message)
+			return
+
+		if resource == "/data":
+			self.send_response(200)
+			self.end_headers()
+			tcsv = json.loads(self.table.get(params["tb"][0])).get("tb")
+			tarray = tcsv.split(",")
+			darray = content.split(",")
+
+			i = 0
+			m = [{"t":params["ts"][0],"lat":params["lat"][0], \
+				"lon":params["lon"][0],"v":""}]
+			videk_json = []
+			for t in tarray:
+				sensor = json.loads(self.sensor.get(t))
+				m[0]["v"] = int(darray[i])
+				sensor["m"] = m
+				m = [{"t":params["ts"][0],"lat":params["lat"][0], \
+					"lon":params["lon"][0],"v":""}]
+				videk_json.append(sensor)
+				i = i + 1
+
+			data = videk_json
 
 			for sensor in data:
 				cluster = sensor["c"]
@@ -71,12 +112,10 @@ class requestHandler(BaseHTTPRequestHandler):
 				s_q = sensor["sq"]
 				s_u = sensor["su"]
 				measurements = sensor["m"]
-
 				self.upload_data(cluster, node, s_t, s_q, s_u, measurements)
-		except:
-			pass
 
-		self.send_response(200)
+			self.wfile.write("done")
+			return
 
 if __name__ == "__main__":
 	main()
