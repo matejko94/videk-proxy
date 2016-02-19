bfdfc101a882e67722c5b03ddc6ee71038b87b41
Matevz Vucnik <vucnikm@gmail.com>
2016-02-19 17:51:28 +0100

    fix travis client hack

--- proxy-client.py
+++ proxy-client.py
@@ -15,25 +15,29 @@ sensor_2 = {"c":"Beep","n":"BeepMislinjaX","st":"SHT22", \
 sensor_3 = {"c":"Beep","n":"BeepMislinjaX","st":"SHT23", \
 	"sq":"temperature","su":"degC"}
 
-s1 = requests.post(url_s, data=json.dumps(sensor_1))
-s2 = requests.post(url_s, data=json.dumps(sensor_2))
-s3 = requests.post(url_s, data=json.dumps(sensor_3))
+res = requests.post(url_s+"test", data=json.dumps(sensor_1))
+print res
+print res.headers
 
-s = s1.text + "," + s2.text + "," + s3.text
-print s
-print s1.headers
-print s2.headers
-print s3.headers
+#s1 = requests.post(url_s, data=json.dumps(sensor_1))
+#s2 = requests.post(url_s, data=json.dumps(sensor_2))
+#s3 = requests.post(url_s, data=json.dumps(sensor_3))
 
-table_data = {"tb":str(s)}
-t = requests.post(url_t, data=json.dumps(table_data))
-print t.text
-print t.headers
+#s = s1.text + "," + s2.text + "," + s3.text
+#print s
+#print s1.headers
+#print s2.headers
+#print s3.headers
 
-d = "1,2,3\n4,5,6\n7,8,9.122"
-time = str(int(time.time()))
-url_d = url_d + "?tb=" + t.text + "&ts=" + time + \
-	"&lat=46.056947" + "&lon=14.505751"
-d = requests.post(url_d, data=d)
-print d.text
-print d.headers
+#table_data = {"tb":str(s)}
+#t = requests.post(url_t, data=json.dumps(table_data))
+#print t.text
+#print t.headers
+
+#d = "1,2,3\n4,5,6\n7,8,9.122"
+#time = str(int(time.time()))
+#url_d = url_d + "?tb=" + t.text + "&ts=" + time + \
+#	"&lat=46.056947" + "&lon=14.505751"
+#d = requests.post(url_d, data=d)
+#print d.text
+#print d.headers
